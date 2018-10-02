ef731140d7ec8f208db229ed81ec7f94da79140e
Alan Chen <agchen@Alans-MBP.lan>
2018-10-02 17:48:28 -0400

    Rewrote script as one cell and started basic cleaning

--- eBayAutoSales.py
+++ eBayAutoSales.py
@@ -12,15 +12,35 @@ autos.head()
 #Data contains a total of 20 columns, most of which are string
 #Some columns contain null values, but none have more than 20% null
 #Column names are presented in camelcase, not snakecase
+
+
 #Strategy...
 #Convert columns names from camelcase to snakecase and rename columns
 #based on the data dictionary to be more descriptive
-autos.columns
 autos = autos.rename(columns={"yearOfRegistration": "registration_year", "monthOfRegistration": "registration_month", 
                               "notRepairedDamage": "unrepaired_damage", "dateCreated": "ad_created", "offerType": "offer_type", 
                               "fuelType": "fuel_type", "dateCrawled": "date_crawled", "nrOfPictures":"number_of_pictures", "postalCode":"postal_code", 
                               "lastSeen":"last_seen", "vehicleType": "vehicle_type", "powerPS" : "power_ps" })
 
 autos.columns
+
 #By changing column names to use snakecase, we can replace spaces with
 #underscores and have an easier way to extract data to look into insights
+
+#Perform some basic data edxploration to determine what cleaning tasks are needed
+#Text columns that are nearly all the same values can be dropped
+#since no insight can gained from that circumstance
+#Columns with numeric data that are stored as tect can be cleaned and converted
+autos.describe(include='all')
+autos['price'].head(50)
+
+
+#FINDINGS:
+#Columns 'seller' and 'offer_type' has nearly the same value for each row.
+#These two will be prime candidates to be dropped first
+#'price' and 'odometer' are represented as objects (hence, strings) rather than integers
+#These two columns should be cleaned and modified
+autos=autos.drop(columns=['seller','offer_type'])
+autos['price']=(autos['price'].str.replace('$','').str.replace(',','').astype(int))
+autos['odometer']=(autos['odometer'].str.replace(',','').str.replace('km','').astype(int))
+#Rename odometer
