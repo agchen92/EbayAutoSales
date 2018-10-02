1c6160bc38693bc9c538e468e6aed2a04b81fc7f
Alan Chen <agchen@Alans-MBP.lan>
2018-10-02 17:31:16 -0400

    Renamed columns to preferred syntax

--- eBayAutoSales.py
+++ eBayAutoSales.py
@@ -16,3 +16,11 @@ autos.head()
 #Convert columns names from camelcase to snakecase and rename columns
 #based on the data dictionary to be more descriptive
 autos.columns
+autos = autos.rename(columns={"yearOfRegistration": "registration_year", "monthOfRegistration": "registration_month", 
+                              "notRepairedDamage": "unrepaired_damage", "dateCreated": "ad_created", "offerType": "offer_type", 
+                              "fuelType": "fuel_type", "dateCrawled": "date_crawled", "nrOfPictures":"number_of_pictures", "postalCode":"postal_code", 
+                              "lastSeen":"last_seen", "vehicleType": "vehicle_type", "powerPS" : "power_ps" })
+
+autos.columns
+#By changing column names to use snakecase, we can replace spaces with
+#underscores and have an easier way to extract data to look into insights
