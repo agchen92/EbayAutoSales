50ca8ee3bfa2259c031f59b17d68acec31fdaaa9
Alan Chen <agchen@Alans-MacBook-Pro.local>
2018-10-03 13:08:30 -0400

    Data exploration changes

--- eBayAutoSales.py
+++ eBayAutoSales.py
@@ -65,3 +65,38 @@ autos['price'].value_counts().sort_index(ascending=True)
 #to encourage viewers to click on the post itself. I will keep these data points.
 #However, there is a few outliers surpassing >=1million USD. I will like to remove
 #these since nothing that valuable would be advertised on eBay.
+rows_price_too_big=(autos['price']>900000)
+autos.drop(autos.index[rows_price_too_big], inplace=True)
+autos['price'].value_counts().sort_index(ascending=True)
+
+autos['registration_year'].unique()
+autos['registration_year'].describe()
+autos['registration_year'].isnull().sum()
+autos['registration_year'].value_counts().sort_index(ascending=True)
+#Some questionable years are found. Let's remove those with years <1800
+#and those greater than 2019.
+questionable_years=(autos['registration_year']<1800) | (autos['registration_year']>2018)
+autos.drop(autos.index[questionable_years], inplace=True)
+autos['registration_year'].value_counts().sort_index(ascending=True)
+#Recheck dataframe
+autos.info()
+autos['registration_year'].value_counts(normalize=True)
+#After removing the values that were outside the upper and lower bounds
+#and then looking at the normalized distribution of the remaining values,
+#we can see that the years from 1998-2006 were the most popular cars to be
+#sold in this classified section
+
+autos['date_crawled']
+autos['ad_created']
+autos['last_seen']
+#With these time stamps, we most likely do not need the HH:MM:SS timestamp
+#So, lets remove these from the 'date_crawled' and 'last_seen' in order to
+#extract the information regarding the years, months, and days.
+
+#Strategy: separate time values into two parts and drop the timestamp or do .str[:10]
+#to just look at the necessary info about year, months, and days.
+autos['date_crawled'].str[:10].value_counts(normalize=True).sort_index(ascending=True)
+#
+autos['ad_created'].str[:10].value_counts(normalize=True).sort_index(ascending=True)
+#
+autos['last_seen'].str[:10].value_counts(normalize=True).sort_index(ascending=True)
